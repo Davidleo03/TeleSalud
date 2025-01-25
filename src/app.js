bee77477e27c2f7d9b3fd2325889cba671576fd8
David López <133236822+Davidleo03@users.noreply.github.com>
2025-01-25 20:46:49 +0000

    version 0.1

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import router from './route.js'
+
+
+const __dirname =  dirname(fileURLToPath(import.meta.url));
+
+
+
+const app = express();
+
+
+app.set('view engine', 'ejs');
+app.set('views',  __dirname + '/views');
+
+app.use('/recursos', express.static(join(__dirname, 'public')));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
+app.use('/', router);
+
+
+const port = 3000 || process.env.PORT;
+
+
+app.listen(port, () => console.log(`Server Running on port ${port}`))
